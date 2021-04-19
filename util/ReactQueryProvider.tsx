4b8f0b826863f3f6db06b56f904f1a4c40f0c4f8
Tony Martinez <ajosephmartinez@gmail.com>
2021-04-19 03:35:17 +0000

    Don't show white screen on auth loading, check if things still work

--- util/ReactQueryProvider.tsx
+++ util/ReactQueryProvider.tsx
@@ -72,7 +72,7 @@ const ReactQueryProvider = ({children, pageProps}: NavProps) => {
     }
   }, [accessToken])
 
-  if (isLoading) return <div>Loading...</div>
+  // if (isLoading) return <div>Loading?</div>
   if (error) return <div>{error.message}</div>
 
   return (
